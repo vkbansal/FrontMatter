689c7585a717a0442dfe8641a32a78bd6ea4949e
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-11-11 16:55:54 +0530

    Updated Specs

--- spec/VKBansal/FrontMatter/ParserSpec.php
+++ spec/VKBansal/FrontMatter/ParserSpec.php
@@ -9,11 +9,19 @@ use VKBansal\FrontMatter\Document;
 class ParserSpec extends ObjectBehavior
 {
     private $sample_header = [
-        'layout' => 'custom',
-        'my_list' => [ 'one', 'two', 'three']
+        "host" => "http://example.com",
+        "port" => 25,
+        "user" => [
+            "name" => [
+                "first" => "John",
+                "last" => "Doe"
+            ],
+            "id" => "johndoe@example.com",
+            "password" => "mypassword"
+        ]
     ];
 
-    private $sample_content = "Main Title\n-----\n### Subtilte\n\nLorem ipsum......";
+    private $sample_content = "Main Title\n-----\n### Subtilte\n\nLorem ipsum......\n";
 
     private $dir;
 
@@ -22,63 +30,54 @@ class ParserSpec extends ObjectBehavior
     function let()
     {
         $this->dir = __DIR__.'/../../../test/resources/';
-        $this->document = new Document('<body>Hello</body>', ['title' => 'test', 'layout' => 'layout.html']);
+        $this->document = new Document($this->sample_content, $this->sample_header);
+    }
+
+    function it_should_parse_yaml_as_default()
+    {   
+        $yaml = file_get_contents($this->dir.'yaml.md');
+        $this->parse($yaml)->getConfig()->shouldReturn($this->sample_header);
+        $this->parse($yaml)->getContent()->shouldReturn($this->sample_content);
+        $this->dump($this->document)->shouldReturn($yaml);      
     }
 
     function it_should_parse_yaml()
-    {
-        $this->parse(file_get_contents($this->dir.'yaml.md'))
-             ->getConfig()
-             ->shouldReturn($this->sample_header);
-        
-        $this->parse(file_get_contents($this->dir.'yaml2.md'))
-             ->getContent()
-             ->shouldReturn($this->sample_content);
+    {   
+        $yaml = file_get_contents($this->dir.'yaml2.md');
+        $this->parse($yaml)->getConfig()->shouldReturn($this->sample_header);
+        $this->parse($yaml)->getContent()->shouldReturn($this->sample_content);
+        $this->dump($this->document, 'yaml')->shouldReturn($yaml);
     }
+
 
     function it_should_parse_json()
-    {
-        $this->parse(file_get_contents($this->dir.'json.md'))
-             ->getConfig()
-             ->shouldReturn($this->sample_header);
-        
-        $this->parse(file_get_contents($this->dir.'json2.md'))
-             ->getContent()
-             ->shouldReturn($this->sample_content);
-    }
+    {   
+        $json = file_get_contents($this->dir.'json.md');
+        $this->parse($json)->getConfig()->shouldReturn($this->sample_header);
+        $this->parse($json)->getContent()->shouldReturn($this->sample_content);
+        $this->dump($this->document, 'json')->shouldReturn($json);
+   }
+
+    function it_should_parse_json_deprecated()
+    {   
+        $json = file_get_contents($this->dir.'json2.md');
+        $this->parse($json)->getConfig()->shouldReturn($this->sample_header);
+        $this->parse($json)->getContent()->shouldReturn($this->sample_content);
+        $this->dump($this->document, true)->shouldReturn($json);
+   }
 
     function it_should_parse_ini()
-    {
-        $this->parse(file_get_contents($this->dir.'ini.md'))
-             ->getConfig()
-             ->shouldReturn($this->sample_header);
-    }
-
-    function it_should_dump_yaml(){
-        $this->dump($this->document)
-             ->shouldReturn("---\ntitle: test\nlayout: layout.html\n---\n<body>Hello</body>");
-    }
-
-    function it_should_dump_json(){
-        $this->dump($this->document, true)
-             ->shouldReturn(";;;\n{\n    \"title\": \"test\",\n    \"layout\": \"layout.html\"\n}\n;;;\n<body>Hello</body>");
-    }
-
-    function it_should_dump_json2()
-    {
-        $this->dump($this->document, 'json')
-             ->shouldReturn("--- json\n{\n    \"title\": \"test\",\n    \"layout\": \"layout.html\"\n}\n---\n<body>Hello</body>");
-    }
-
-    function it_should_dump_ini()
-    {
-        $this->dump($this->document, 'ini')
-             ->shouldReturn("--- ini\ntitle = test\nlayout = layout.html\n---\n<body>Hello</body>");   
-    }
+    {   
+        $ini = file_get_contents($this->dir.'ini.md');
+        $this->parse($ini)->getConfig()->shouldBeLike($this->sample_header);
+        $this->parse($ini)->getContent()->shouldReturn($this->sample_content);
+        $this->dump($this->document, 'ini')->shouldReturn($ini);
+   }
 
     function it_should_validate_automatically(){
         $this->isValid(file_get_contents($this->dir.'yaml.md'))->shouldBeTrue();
         $this->isValid(file_get_contents($this->dir.'json.md'))->shouldBeTrue();
+        $this->isValid(file_get_contents($this->dir.'ini.md'))->shouldBeTrue();
         $this->isValid('Lorem Ipsum')->shouldBeFalse();
     }
 
